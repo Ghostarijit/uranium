c72653a7e566f34e3d80ff74ad1652c89d9b83b3
Ghostarijit <arijitbiswas484@gmail.com>
2022-04-26 11:37:54 +0530

    my change

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,7 +1,7 @@
 let axios = require("axios")
 
 
-let getStates = async function (req, res) {
+let getStates = async function(req, res) {
 
     try {
         let options = {
@@ -12,15 +12,14 @@ let getStates = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
 
-let getDistricts = async function (req, res) {
+let getDistricts = async function(req, res) {
     try {
         let id = req.params.stateId
         let options = {
@@ -31,14 +30,13 @@ let getDistricts = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getByPin = async function (req, res) {
+let getByPin = async function(req, res) {
     try {
         let pin = req.query.pincode
         let date = req.query.date
@@ -50,17 +48,16 @@ let getByPin = async function (req, res) {
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getOtp = async function (req, res) {
+let getOtp = async function(req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -71,15 +68,91 @@ let getOtp = async function (req, res) {
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
 
+
+let getSession = async function(req, res) {
+    try {
+        let id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${id} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+let getSortedCities = async function(req, res) {
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        let cityObjArray = []
+        for (i = 0; i < cities.length; i++) {
+            let obj = { city: cities[i] }
+            let arijit = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&APPID=c616c98caf8e47199b5bcfe6538a1a5c`)
+
+            console.log(arijit.data.main.temp)
+
+            obj.temp = arijit.data.main.temp
+
+            cityObjArray.push(obj)
+
+
+        }
+
+        let shorted = cityObjArray.sort(function(a, b) { a.temp - b.temp })
+        console.log(shorted)
+        res.status(200).send({ status: true, deta: shorted })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+let getmemes = async function(req, res) {
+    try {
+        let template_id = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let username = req.query.username
+        let password = req.query.password
+
+
+
+
+
+        var options = {
+            method: "post",
+            url: `http://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`,
+
+        }
+
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+
+module.exports.getSession = getSession
+module.exports.getSortedCities = getSortedCities
+
+module.exports.getmemes = getmemes

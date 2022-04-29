08180d2638d36a5b6d6c22e994bb608a19df5a07
Ghostarijit <arijitbiswas484@gmail.com>
2022-04-29 15:14:43 +0530

    aythorization

--- src/Middleware/Middleware.js
+++ src/Middleware/Middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const blogModel = require("../model/blogModel");
 const validateToken = async function(req, res, next) {
     try {
         let token = req.headers['x-api-key'] || req.headers['x-Api-key']
@@ -23,13 +24,13 @@ const validateToken = async function(req, res, next) {
 const authorization = async function(req, res, next) {
     try {
         let token = req.headers['x-api-key'] || req.headers['x-Api-key']
-        let authorId = req.params.authorId || req.query.authorId || req.body.authorId || req.headers.authorId
-
+        let BlogId = req.params.BlogId || req.query.BlogId || req.body.BlogId || req.headers.BlogId
 
         const decodedtoken = jwt.verify(token, "group-12-project-1")
-        if (!decodedtoken)
-            return res.status(401).send({ status: false, msg: "token is invalid" });
+        if (!decodedtoken) return res.status(401).send({ status: false, msg: "token is invalid" });
 
+        const blog = await blogModel.findOne(BlogId)
+        let authorId = blog.authorId
         const id = decodedtoken.authorId
         if (authorId != id)
             return res.status(403).send({ status: false, msg: "cannot access" });
